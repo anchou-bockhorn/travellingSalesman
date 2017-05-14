6ab6d9837000c540254f7b90c5af314cc8db8259
Anchou Bockhorn <abockhorn@tocco.ch>
2017-05-14 09:31:27 +0200

    best path finder impl draft

--- src/main/java/life/connect_it/travellingsalesman/pathfinder/BestPathFinder.java
+++ src/main/java/life/connect_it/travellingsalesman/pathfinder/BestPathFinder.java
@@ -3,14 +3,41 @@ package life.connect_it.travellingsalesman.pathfinder;
 import java.util.ArrayList;
 
 import life.connect_it.travellingsalesman.map.SalesManMap;
+import life.connect_it.travellingsalesman.map.SalesManMapFactory;
 import life.connect_it.travellingsalesman.salespoint.salespointimpl.SalesPoint;
 
 public class BestPathFinder implements PathFinder {
+    private static final SalesManMapFactory MAP_FACTORY = new SalesManMapFactory();
+
+    private final SalesManMap basicMap;
+    private final SalesManMap uuidMap;
+
+    public BestPathFinder() {
+        this.basicMap = MAP_FACTORY.getSalesManMap(null);
+        this.uuidMap = MAP_FACTORY.getSalesManMapUuid(null);
+    }
 
     @Override
     public ArrayList<SalesPoint> findPath(SalesManMap salesManMap) {
-        ArrayList<ArrayList<SalesPoint>> witnesses = salesManMap.getWitnesses();
+        ArrayList<ArrayList<Integer>> witnesses = salesManMap.getWitnesses();
+
+        ArrayList<Integer> currentlyBestPath = witnesses.get(0);
+        double bestDistance = calculateDistance(currentlyBestPath, salesManMap);
+
+        for (int i = 1; i < witnesses.size(); i++) {
+            if (bestDistance > calculateDistance(witnesses.get(i), salesManMap)) {
+                currentlyBestPath = witnesses.get(i);
+            }
+        }
 
         return null;
+    }
+
+    private double calculateDistance(ArrayList<Integer> path, SalesManMap map) {
+        double distance = 0.0;
+        for (int i = 0; i < path.size() - 1; i++) {
+            distance += map.getSalesPoint(path.get(i)).getTargetDistance(map.getSalesPoint(path.get(i + 1)));
+        }
+        return distance;
     }
 }
